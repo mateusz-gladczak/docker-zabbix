0c84b0b48e5177601f26ec57c96a3cefa1f2741f
Mateusz <mateusz@gladczak.com>
2019-05-14 07:39:50 +0200

    konfiguracja

---
+++ docker_install.sh
@@ -0,0 +1,10 @@
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y remove docker docker-engine docker.io containerd runc
+sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt -y update
+sudo apt -y install docker-ce docker-ce-cli containerd.io
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
